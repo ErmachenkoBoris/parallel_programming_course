41f30d7e53ba7e7c8cae76ad7e75f4a557fa66d0
Boris Ermachenko <cbkf12.ru@yandex.ru>
2019-03-06 22:42:33 +0300

    ermachenko filtr gauss 1

--- modules/task_1/ermachenko_b_filtr_gauss_block_omp/main.cpp
+++ modules/task_1/ermachenko_b_filtr_gauss_block_omp/main.cpp
@@ -2,89 +2,88 @@
 #include <math.h>
 #include <ctime>
 #include<iostream>
-//#pragma comment(linker, "/STACK:8000000")
-//#pragma comment(linker, "/HEAP:8000000")
-using namespace std;
+// #pragma comment(linker, "/STACK:8000000")
+// #pragma comment(linker, "/HEAP:8000000")
 const int PI = 3.1415;
 double**& getNewArr(int width, int height) {
-	double** new_arrImage = new double*[height];
-	for (int i = 0; i < height; i++) {
-		new_arrImage[i] = new double[width];
-	}
-	return new_arrImage;
+    double** new_arrImage = new double*[height];
+    for (int i = 0; i < height; i++) {
+        new_arrImage[i] = new double[width];
+    }
+    return new_arrImage;
 }
 double** FilterGauss(double** arrImage, double** new_arrImage, int width, int height) {
-	double sum = 0;
-	double koef1 = 1.398;
-	double koef2 = 2 * koef1*koef1;
-	int n = 3;
-	double koef3;
-	double color;
-	double** w = getNewArr(n, n);
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			koef3 = (i*i + j * j) / koef2;
-			w[i][j] = 1 / (sqrt(PI*koef2))*exp(koef3*(-1.0));
-			// sum += w[i][j];
-		}
-		// for (int j = 0; j < n; j++)
-		//{
-			//w[i][j] = w[i][j] /sum;
-		//}
-	}
-	for (int i = 0; i < height; i++) {
-		for (int j = 0; j < width; j++) {
-			new_arrImage[i][j] = arrImage[i][j];
-		}
-	}
-	for (int yi = n; yi < height - n; yi++)
-	{
-		for (int xj = n; xj < width - n; xj++)
-		{
-			for (int i = 0; i < n; i++)
-			{
-				color = 0;
-				for (int j = 0; j < n; j++)
-				{
-					color += w[i][j] * arrImage[(int)(i + yi)][(int)(j + xj)];
-				}
-			}
-			new_arrImage[(int)(yi)][(int)(xj)] = color;
-		}
-	}
-	return new_arrImage;
+    double sum = 0;
+    double koef1 = 1.398;
+    double koef2 = 2 * koef1*koef1;
+    int n = 3;
+    double koef3;
+    double color;
+    double** w = getNewArr(n, n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            koef3 = (i*i + j * j) / koef2;
+            w[i][j] = 1 / (sqrt(PI*koef2))*exp(koef3*(-1.0));
+            // sum += w[i][j];
+        }
+        // for (int j = 0; j < n; j++)
+        // {
+        // w[i][j] = w[i][j] /sum;
+        // }
+    }
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            new_arrImage[i][j] = arrImage[i][j];
+        }
+    }
+    for (int yi = n; yi < height - n; yi++)
+    {
+        for (int xj = n; xj < width - n; xj++)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                color = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    color += w[i][j] * arrImage[static_cast<int>(i + yi)][static_cast<int>(j + xj)];
+                }
+            }
+            new_arrImage[static_cast<int>(yi)][static_cast<int>(xj)] = color;
+        }
+    }
+    return new_arrImage;
 }
 
 double** getImage(int width, int height) {
-				srand(time(0));
-				double** arrImage = getNewArr(width, height);
-	for (int i = 0; i < height; i++) {
-		for (int j = 0; j < width; j++) {
-			arrImage[i][j] = (int)rand() % 256;
-		}
-	}
-	return arrImage;
+    srand(time(0));
+    double** arrImage = getNewArr(width, height);
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            arrImage[i][j] = static_cast<int>(rand()) % 256;
+        }
+    }
+    return arrImage;
 }
-void ShowArr(double** arr,int width,int height) {
-	// std::cout.precision(0);
-	cout << "\n";
-	for (int i = 0; i < height; i++) {
-		for (int j = 0; j < width; j++) {
-			//cout.precision(0);
-			cout << arr[i][j] << " ";
-		}
-		cout << "\n";
-	}
+void ShowArr(double** arr, int width, int height) {
+    // std::cout.precision(0);
+    std::cout << "\n";
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            // cout.precision(0);
+            std::cout << arr[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
 }
-int main(){
-	int height = 800;
-	int width = 400;
-	double** arrImage = getImage(width,height);
-	double** new_arrImage = getNewArr(width, height);
-	new_arrImage = FilterGauss(arrImage, new_arrImage, width, height);
-	ShowArr(arrImage, width, height);
-	ShowArr(new_arrImage, width, height);
+int main() {
+    int height = 800;
+    int width = 400;
+    double** arrImage = getImage(width, height);
+    double** new_arrImage = getNewArr(width, height);
+    new_arrImage = FilterGauss(arrImage, new_arrImage, width, height);
+    ShowArr(arrImage, width, height);
+    ShowArr(new_arrImage, width, height);
     return 0;
 }
