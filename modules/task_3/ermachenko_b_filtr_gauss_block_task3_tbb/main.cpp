310dce445a021ca969e0987b193589ff2755c7ac
borisssta <cbkf12.ru@yandex.ru>
2019-04-22 16:13:09 +0300

    task 3 2.2

--- modules/task_3/ermachenko_b_filtr_gauss_block_task3_tbb/main.cpp
+++ modules/task_3/ermachenko_b_filtr_gauss_block_task3_tbb/main.cpp
@@ -58,7 +58,7 @@ double** LinierFilterGauss(double** arrImage, double** new_arrImage,
     return new_arrImage;
 }
 void getBlockNum(int throwds) {
-    int tmp = sqrt(throwds);
+    int tmp = (int)sqrt(throwds);
     while (throwds % tmp != 0) {
         tmp--;
     }
@@ -96,75 +96,6 @@ int * arrayLengthBlocksY(int height) {
         }
     }
     return rezArrY;
-}
-double** ParallelFilterGaussOMP(double** arrImage,
-    double** new_arrImage, int width, int height) {
-    int n = 3;
-    double** w = getNewArr(n, n);
-    double sigma = 1.0;
-    double r;
-    double s = 2.0 * sigma * sigma;
-    for (int i = -n / 2; i < n / 2; i++) {
-        for (int j = -n / 2; j < n / 2; j++) {
-            r = sqrt(i * i + j * j);
-            w[i + n / 2][j + n / 2] = (exp(-(r*r) / s)) / (PI * s);
-        }
-    }
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            new_arrImage[i][j] = arrImage[i][j];
-        }
-    }
-    getBlockNum(THREADS);
-    int* lengthX = arrayLengthBlocksX(width);
-    int* lengthY = arrayLengthBlocksY(height);
-#pragma omp parallel
-    {
-        int numberThroat = omp_get_thread_num();
-        int startX = 0;
-        int startY = 0;
-        // int RestX = 0;
-        int row = 0;
-        int count = 0;
-        int column = 0;
-        for (int i = 0; i < blockY; i++) {
-            if (i > 0) startY += lengthY[i - 1];
-            for (int j = 0; j < blockX; j++) {
-                if (j > 0) startX += lengthX[j - 1];
-                count++;
-                if (count > numberThroat)break;
-                column++;
-            }
-            if (count > numberThroat)break;
-            row++;
-            column = 0;
-            startX = 0;
-        }
-        for (int yi = startY; yi < startY + lengthY[row]; yi++) {
-            for (int xj = startX; xj < startX + lengthX[column]; xj++) {
-                double color = 0;
-                double kSum = 0;
-                for (int i = -n / 2; i < n / 2; i++) {
-                    for (int j = -n / 2; j < n / 2; j++) {
-                        kSum += w[i + n / 2][j + n / 2];
-                        if ((i + yi) >= 0 && (i + yi) < height
-                            && (j + xj) >= 0 && (j + xj) < width)
-                            color += arrImage[static_cast<int>(i + yi)][static_cast<int>(j + xj)]
-                            * w[i + n / 2][j + n / 2];
-                    }
-                }
-                if (kSum <= 0) kSum = 1;
-                color /= kSum;
-                if (color < 0) color = 0;
-                if (color > 255) color = 255;
-                if ((yi) >= 0 && (yi) < height &&
-                    (xj) >= 0 && (xj) < width)
-                    new_arrImage[static_cast<int>(yi)][static_cast<int>(xj)]
-                    = color;
-            }
-        }
-    }
-    return new_arrImage;
 }
 double** ParallelFilterGaussTBB(double** arrImage,
     double** new_arrImage, int width, int height) {
@@ -245,15 +176,6 @@ int main() {
     tEnd = omp_get_wtime();
     float tmp = tEnd - tStart;
     printf("Time linier :  %.4lf \n", tEnd - tStart);
-    // OpenMp
-    double** new_arrImage_Parallel = getNewArr(width, height);
-    tStart = omp_get_wtime();
-    new_arrImage_Parallel = ParallelFilterGaussOMP(arrImage,
-       new_arrImage_Parallel, width, height);
-    tEnd = omp_get_wtime();
-    printf("Time parallel OpenMp:  %.4lf \n", tEnd - tStart);
-    printf("Koef :  %.4lf \n", tmp / (tEnd - tStart));
-    // tbb
     tbb::task_scheduler_init init(tbb::task_scheduler_init::deferred);
     init.initialize(THREADS);
     double** new_arrImage_Parallel_TBB = getNewArr(width, height);
